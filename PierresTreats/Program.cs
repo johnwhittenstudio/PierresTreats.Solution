8f0e5e883a193b2e245530f26cafecaf24809a54
John Whitten <johnwhitten.studio@gmail.com>
2022-04-07 10:32:44 -0700

    create csproj and Program + Startup

--- PierresTreats/Program.cs
+++ PierresTreats/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace PierresTreats.Solution
+namespace PierresTreats
 {
     public class Program
     {
